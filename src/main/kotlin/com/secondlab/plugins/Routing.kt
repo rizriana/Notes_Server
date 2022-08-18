80dc79e3ce9771e55cc7e112fbb2a3bc1e2d8d86
Rizki Rian Anandita <65359346+rizriana@users.noreply.github.com>
2022-08-19 00:56:02 +0700

    Create route for call the token

--- src/main/kotlin/com/secondlab/plugins/Routing.kt
+++ src/main/kotlin/com/secondlab/plugins/Routing.kt
@@ -1,12 +1,19 @@
 package com.secondlab.plugins
 
-import com.secondlab.repository.DatabaseFactory
+import com.secondlab.authentication.JwtService
+import com.secondlab.authentication.hash
+import com.secondlab.data.model.User
+import com.secondlab.repository.Repository
 import io.ktor.application.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
+
+    val db = Repository()
+    val jwtService = JwtService()
+    val hashFunction = { s:String -> hash(s) }
 
     routing {
         get("/") {
@@ -17,6 +24,15 @@ fun Application.configureRouting() {
         get("/note/{id}") {
             val id = call.parameters["id"]
             call.respond("$id")
+        }
+
+        get("/token"){
+            val email = call.request.queryParameters["email"]!!
+            val password = call.request.queryParameters["password"]!!
+            val username = call.request.queryParameters["username"]!!
+
+            val user = User(email,hashFunction(password),username)
+            call.respond(jwtService.generateToken(user))
         }
 
         // query parameter - localhost:8080/note?id=value
