a091ecc80147076f5142093d5f069486b00af275
Rizki Rian Anandita <65359346+rizriana@users.noreply.github.com>
2022-08-20 01:23:50 +0700

    Adding "jwt" authentication verifier

--- src/main/kotlin/com/secondlab/plugins/Security.kt
+++ src/main/kotlin/com/secondlab/plugins/Security.kt
@@ -1,35 +1,32 @@
 package com.secondlab.plugins
 
-import io.ktor.auth.*
-import io.ktor.util.*
-import io.ktor.auth.jwt.*
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.sessions.*
+import com.secondlab.authentication.JwtService
+import com.secondlab.repository.Repository
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
+import io.ktor.sessions.*
+import kotlin.collections.set
 
 fun Application.configureSecurity() {
-    
+
+    val db = Repository()
+    val jwtService = JwtService()
+
     authentication {
-            jwt {
-                val jwtAudience = environment.config.property("jwt.audience").getString()
-                realm = environment.config.property("jwt.realm").getString()
-                verifier(
-                    JWT
-                        .require(Algorithm.HMAC256("secret"))
-                        .withAudience(jwtAudience)
-                        .withIssuer(environment.config.property("jwt.domain").getString())
-                        .build()
-                )
-                validate { credential ->
-                    if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
-                }
+        jwt("jwt") {
+            verifier(jwtService.varifier)
+            realm = "Note Server"
+            validate {
+                val payload = it.payload
+                val email = payload.getClaim("email").asString()
+                val user = db.findUserByEmail(email)
+                user
             }
         }
+    }
     data class MySession(val count: Int = 0)
     install(Sessions) {
         cookie<MySession>("MY_SESSION") {
@@ -39,9 +36,9 @@ fun Application.configureSecurity() {
 
     routing {
         get("/session/increment") {
-                val session = call.sessions.get<MySession>() ?: MySession()
-                call.sessions.set(session.copy(count = session.count + 1))
-                call.respondText("Counter is ${session.count}. Refresh to increment.")
-            }
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
     }
 }
